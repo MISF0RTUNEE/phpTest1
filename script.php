682d2316337d5c7b1e661e0d26e2fa89cccaefe7
MISF0RTUNEE <dimakom2000@gmail.com>
2024-11-11 04:13:10 +0300

    123

--- script.php
+++ script.php
@@ -4,105 +4,123 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $query = trim($_POST['query']);
     $region = $_POST['region'];
     $token = $_POST['token'];
-    $lines = explode("\n", $query);
-    $lines = array_map("trim", $lines);
-}
-    echo "</ul>";
-$url = 'https://api-sandbox.direct.yandex.ru/v4/json/';
+    $lines = array_map("trim", explode("\n", $query));
 
-$headers = array(
-    'Content-Type' => 'application/json; charset=utf-8'    
-);
-
-$data = array(
-    "method" => "CreateNewWordstatReport",
-    "param" => array( 
-        'Phrases' => $lines,
-        'GeoID' => array($region),
-    ),
-    'locale' => 'ru', 
-    'token' => $token
-);
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data,JSON_UNESCAPED_UNICODE));
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
-$result = curl_exec($ch);
-curl_close($ch);
+    $url = 'https://api-sandbox.direct.yandex.ru/v4/json/';
+    $headers = array('Content-Type' => 'application/json; charset=utf-8');
 
 
-$reportId = implode(json_decode($result, true));
-print_r($reportId);
+    $chunks = array_chunk($lines, 10);
+    $reportIds = [];
 
-$data = array(
-    "method" => "GetWordstatReport",
-    "param" => $reportId,
-    'locale' => 'ru',
-    'token' => $token
-);
+    foreach ($chunks as $chunk) {
+        $data = array(
+            "method" => "CreateNewWordstatReport",
+            "param" => array( 
+                'Phrases' => $chunk,
+                'GeoID' => array($region),
+            ),
+            'locale' => 'ru', 
+            'token' => $token
+        );
 
-$maxRetries = 10;
-$retryCount = 0;
-do{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data, JSON_UNESCAPED_UNICODE));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
+        $result = curl_exec($ch);
 
-    $result = curl_exec($ch);
-    curl_close($ch);
+        curl_close($ch);
 
-    $responseData = json_decode($result, true);
-
-    if (isset($responseData['data'])) {
-        break;
-    } else {
-        sleep(5);
-        $retryCount++;
+        $reportId = json_decode($result, true)['data'] ?? null;
+        if ($reportId) {
+            $reportIds[] = $reportId;
+        }
+        usleep(500000);
     }
-} while ($retryCount < $maxRetries);
+
+    $maxRetries = 10;  
+    $retryCount = 0;   
+    $results = [];
+
+    while (!empty($reportIds)) {
+        $currentBatch = array_splice($reportIds, 0, 5); 
+        foreach ($currentBatch as $reportId) {
+            $data = array(
+                "method" => "GetWordstatReport",
+                "param" => $reportId,
+                'locale' => 'ru',
+                'token' => $token
+            );
+            do{
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+                $result = curl_exec($ch);
+                curl_close($ch);
+
+                $responseData = json_decode($result, true);
+                if (isset($responseData['data'])) {
+                    break;
+                } else {
+                    sleep(5);
+                    $retryCount++;
+                }
+            } while($retryCount < $maxRetries);
 
 
-if (isset($responseData['data']) && !empty($responseData['data'])) {
+            if (isset($responseData['data']) && !empty($responseData['data'])) {
+                foreach ($responseData['data'] as $dataItem) {                    
+                    foreach ($dataItem['SearchedWith'] as $searchedWithItem) {
+                        if (in_array($searchedWithItem['Phrase'], $lines)) {
+                            if (isset($searchedWithItem['Shows'])) {
+                                $results[] = [
+                                    'phrase' => $searchedWithItem['Phrase'], 
+                                    'shows' => $searchedWithItem['Shows'],   
+                                ];
+                            }
+                        }
+                    }
+                }
+            } else {
+                echo "Нет данных для обработки." . PHP_EOL;
+            }
+            
+        }
+    }
+
+
+
     $csvFile = 'report_data.csv';
     $fp = fopen($csvFile, 'w');
 
-    fputcsv($fp, ['Запрос', 'Частота']);
+    fputcsv($fp, ['phrase', 'shows'], ';');
 
-    $options = [
-        'delimiter' => ';',
-    ];
-    foreach ($responseData['data'] as $dataItem) {
-        if (isset($dataItem['SearchedAlso'])) {
-            foreach ($dataItem['SearchedAlso'] as $searchedItem) {
-                $phrase = isset($searchedItem['Phrase']) ? $searchedItem['Phrase'] : '';
-                $shows = isset($searchedItem['Shows']) ? $searchedItem['Shows'] : '';
-                fputcsv($fp, [$phrase, $shows], $options['delimiter']);
-            }
+    foreach ($results as $resultItem) {
+        if (isset($resultItem['phrase']) && isset($resultItem['shows'])) {
+            $phrase = mb_convert_encoding($resultItem['phrase'], 'Windows-1251', 'UTF-8');
+            $shows = $resultItem['shows'];
+            fputcsv($fp, [$phrase, $shows], ';');
         }
     }
     fclose($fp);
 
-    header('Content-Type: text/csv; charset=utf-8');
-    header('Content-Disposition: attachment; filename="' . basename($csvFile) . '"');
-    
+    header('Content-Type: text/csv; charset=Windows-1251');
+    header('Content-Disposition: attachment; filename="'. basename($csvFile) .'"');
 
     readfile($csvFile);
-
     unlink($csvFile);
     exit;
-} else {
-    echo "Не удалось получить данные отчета.";
 }
 ?>
